3e84e7164a8b1e637c0f22580369767811f89550
daniel.souza <daniel.souza@grupoicts.com.br>
2024-04-05 15:25:22 -0400

    refactor: removing old role update

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,13 +1,9 @@
 import { Router } from 'express';
 
-import { authMiddleware } from '../middlewares/authMiddleware';
-import { roleMiddleware } from '../middlewares/roleMiddleware';
-
-import { loginUser, updateRole } from '../controllers/authController'
+import { loginUser } from '../controllers/authController'
 
 const router = Router();
 
 router.post('/login', loginUser);
-router.patch('/role/:id', authMiddleware, roleMiddleware(['admin', 'superAdmin']), updateRole);
 
 export default router;
