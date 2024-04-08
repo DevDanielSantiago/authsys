ebc04efdf09716f02e9ea6f58d6caac8b210253c
daniel.souza <daniel.souza@grupoicts.com.br>
2024-04-08 08:16:00 -0400

    refactor: adding permission middleware to permission routes

--- src/routes/permissionRoutes.ts
+++ src/routes/permissionRoutes.ts
@@ -1,15 +1,16 @@
 import { Router } from 'express';
 
 import { authMiddleware } from '../middlewares/authMiddleware';
+import { roleMiddleware } from '../middlewares/roleMiddleware';
 
 import { createPermission, listPermissions, updatePermission, deletePermission, restorePermission } from '../controllers/permissionController'
 
 const router = Router();
 
-router.post('/', createPermission);
-router.get('/', authMiddleware, listPermissions);
-router.patch('/:id', authMiddleware, updatePermission);
-router.delete('/:id', authMiddleware, deletePermission);
-router.post('/restore/:id', authMiddleware, restorePermission);
+router.post('/', authMiddleware, roleMiddleware('create_permission'), createPermission);
+router.get('/', authMiddleware, roleMiddleware('list_permissions'), listPermissions);
+router.patch('/:id', authMiddleware, roleMiddleware('update_permission'), updatePermission);
+router.delete('/:id', authMiddleware, roleMiddleware('delete_permission'), deletePermission);
+router.post('/restore/:id', authMiddleware, roleMiddleware('restore_permission'), restorePermission);
 
 export default router;
